9f6715a500d95df73e43ce2acfbe2385bd32e9b4
gabecg@gmail.com <gabecg@gmail.com>
2023-01-25 10:29:44 -0800

    homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-// import {useState} from "react";
+import {useState} from "react";
 import {
   NavLink,
    Route,
@@ -13,26 +13,64 @@ import {
     Contact
 } from "./index.js";
 
-function App() {
-  return (
-    <main>
-      <h1>Hello</h1>
-      <div className="navbar">
 
-      <ul>
-        <NavLink exact to='/home' >
+
+function App() {
+  const [isActive, setActive] = useState('nav-menu');
+  const [hamburger, setHamburger] = useState('hamburger');
+  
+  const toggleHamburger = () => {
+          
+  
+    if(hamburger === 'hamburger' ){
+    setHamburger('hamburger-active');
+  
+    console.log(isActive)
+    }
+    else
+    setHamburger('hamburger');
+    
+    if(isActive === 'nav-menu'){
+        setActive('nav-menuActive');
+  
+    }else{
+        setActive('nav-menu');
+  
+    }
+  };
+  const closeHamburger= () =>{
+    if(hamburger === 'hamburger'){
+        return null
+    }
+    setHamburger('hamburger');
+    setActive('nav-menu');
+  
+  }
+
+
+  return (
+    <main onClick={closeHamburger}>
+      <div className="navbar">
+        <h4>Gabriel Guild</h4>
+      <ul className={isActive}>
+        <NavLink exact to='/home' className='navlink' activeClassName="active">
           Home
         </NavLink>
-        <NavLink exact to='/about'>
-          about me 
+        <NavLink exact to='/about' className='navlink' activeClassName="active">
+          About 
         </NavLink>
-        <NavLink exact to='/experience'>
+        <NavLink exact to='/experience' className='navlink' activeClassName="active">
         Experience 
         </NavLink>
-        <NavLink exact to='/contact'>
+        <NavLink exact to='/contact' className='navlink' activeClassName="active">
           Contact
         </NavLink>
       </ul>
+      <div className={hamburger} onClick={toggleHamburger}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+    </div>
       </div>
       <Routes>
        <Route path="/Home" element={<Home/>} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const Home =() => {
 return (
-    <div>
-
-    </div>
+    <div class="wrapper">
+    <div class="static-txt">I'm a</div>
+    <ul class="dynamic-txts">
+      <li><span>Father</span></li>
+      <li><span>Designer</span></li>
+      <li><span>Developer</span></li>
+      <li><span>Freelancer</span></li>
+    </ul>
+  </div>
 )
 }
 export default Home;
